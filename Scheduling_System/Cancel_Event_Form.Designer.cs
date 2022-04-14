c21a3577af6361ee32dded6fe1a0f7f21a7ba7bf
Trevone Mckie <trevonemckie@hotmail.com>
2022-04-14 15:08:28 -0400

    Added functionality to the cancel event button

---
+++ Scheduling_System/Cancel_Event_Form.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace Scheduling_System
+{
+    partial class Cancel_Event_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Cancel_Event_Form));
+            this.listBox_Delete_Event = new System.Windows.Forms.ListBox();
+            this.button_delete = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox_Delete_Event
+            // 
+            this.listBox_Delete_Event.FormattingEnabled = true;
+            this.listBox_Delete_Event.ItemHeight = 16;
+            this.listBox_Delete_Event.Location = new System.Drawing.Point(117, 78);
+            this.listBox_Delete_Event.Name = "listBox_Delete_Event";
+            this.listBox_Delete_Event.Size = new System.Drawing.Size(358, 260);
+            this.listBox_Delete_Event.TabIndex = 0;
+            // 
+            // button_delete
+            // 
+            this.button_delete.BackColor = System.Drawing.SystemColors.Control;
+            this.button_delete.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button_delete.BackgroundImage")));
+            this.button_delete.FlatAppearance.BorderSize = 0;
+            this.button_delete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_delete.ForeColor = System.Drawing.SystemColors.Control;
+            this.button_delete.Location = new System.Drawing.Point(581, 284);
+            this.button_delete.Name = "button_delete";
+            this.button_delete.Size = new System.Drawing.Size(99, 54);
+            this.button_delete.TabIndex = 1;
+            this.button_delete.Text = "Delete";
+            this.button_delete.UseVisualStyleBackColor = false;
+            this.button_delete.Click += new System.EventHandler(this.button_delete_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SteelBlue;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(765, 1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(25, 26);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Cancel_Event_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(793, 507);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button_delete);
+            this.Controls.Add(this.listBox_Delete_Event);
+            this.DoubleBuffered = true;
+            this.Name = "Cancel_Event_Form";
+            this.Text = "Cancel_Event_Form";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox_Delete_Event;
+        private System.Windows.Forms.Button button_delete;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Scheduling_System/Cancel_Event_Form.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace Scheduling_System
+{
+    public partial class Cancel_Event_Form : Form
+    {
+        List<string> EventIDs = new List<string>();      // List of customer IDs (used to figure out which person to remove from list/file)
+        public Cancel_Event_Form()
+        {
+            InitializeComponent();
+            ShowEventID();
+        }
+        public void ShowEventID()
+        {
+            foreach (Event ev in Form_Login.eventList)
+            {
+                string line = ev.EventID;      // Only show Event ID
+                listBox_Delete_Event.Items.Add(line);
+                EventIDs.Add(ev.EventID);                      // Keep track of IDs
+            }
+        }
+
+        private void button_delete_Click(object sender, EventArgs e)
+        {
+            if (listBox_Delete_Event.SelectedItem != null)
+            {
+                string selectedItem = listBox_Delete_Event.SelectedItem.ToString();          // What the user selected on the listbox
+                int indexOfSelectedItem = listBox_Delete_Event.SelectedIndex;
+                listBox_Delete_Event.Items.Remove(listBox_Delete_Event.SelectedItem);     // Remove from listbox
+
+                // Remove from list of customers
+                foreach (Event ev in Form_Login.eventList)
+                {
+                    if (ev.EventID == EventIDs.ElementAt(indexOfSelectedItem))     // Remove customer based on customer ID
+                    {
+                        Form_Login.eventList.Remove(ev);
+                        break;
+                    }
+                }
+
+                // Remove from file
+                string[] oldLines = File.ReadAllLines("EventFile.txt");
+                string[] newLines = new string[oldLines.Length - 1];
+                int index = 0;
+                foreach (string line in oldLines)
+                {
+                    if (line == null)
+                        break;
+                    string[] lineInfo = line.Split(',');
+                    if (lineInfo[0] == EventIDs.ElementAt(indexOfSelectedItem))      // If customer ID of selected customer found, remove line from file
+                        continue;
+                    newLines[index] = line;
+                    index++;
+                }
+
+                File.WriteAllLines("ClientFile.txt", newLines);     // Copy into the Client File
+                EventIDs.Remove(EventIDs.ElementAt(indexOfSelectedItem));
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
